40e8ec12c92d4c55da0bf151d045d68e934460e4
Ricardo Correa B. de Moraes <87211904+ricardocbm@users.noreply.github.com>
2023-11-04 21:31:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
 
-Com javascript, crie uma lista de tres **estudantes**  e, cada estudante dentro dessa lista, deverá conter os seguintes dados:
+Crie uma lista de **estudantes** e, cada estudante dentro dessa lista, deverá conter os seguintes dados:
 
 - nome;
 - nota da primeira prova;
@@ -9,37 +9,46 @@ Com javascript, crie uma lista de tres **estudantes**  e, cada estudante dentro 
 Depois de criada a lista:
 
 - [ ]  Crie uma função que irá calcular a média das notas de cada aluno;
-- [ ]  Supondo que a média, para esse concurso é 7, verifique se cada aluno obteve sucesso ou não em entrar no concurso e mostre uma mensagem na tela
+- [ ]  Supondo que a média, para esse concurso é 7, verifique se cada aluno obteve sucesso ou não em entrar no concurso
+ e mostre uma mensagem na tela
 
 */
 
-// Criar uma lista de estudantes
 const estudantes = [
-  { nome: "Maria", nota1: 8, nota2: 6 },
-  { nome: "Wandierdson", nota1: 7, nota2: 8 },
-  { nome: "Capita Marvel", nota1: 6, nota2: 9 },
+  {
+    nome: "Maria",
+    nota1: 10,
+    nota2: 9,
+  },
+
+  {
+    nome: "João",
+    nota1: 4,
+    nota2: 7,
+  },
+
+  {
+    nome: "Carlos",
+    nota1: 4,
+    nota2: 7,
+  },
 ];
 
-// Função para calcular a média das notas de um estudante
-function calcularMedia(estudante) {
-  return ((estudante.nota1 + estudante.nota2) / 2).toFixed(2);
+// Função que gera uma string com a média do estudante
+function printMediaEstudante(estudante) {
+  return `
+Estudante ${estudante.nome} teve média ${
+    (estudante.nota1 + estudante.nota2) / 2
+  }`;
 }
 
-// Função para verificar se um estudante obteve sucesso
-function verificarSucesso(estudante) {
-  const media = calcularMedia(estudante);
-  if (media >= 7) {
-    return `${estudante.nome} teve média ${media.toFixed(
-      2
-    )} e passou no concurso!`;
+// Loop para cada estudante no array estudantes
+for (let estudante of estudantes) {
+  if ((estudante.nota1 + estudante.nota2) / 2 >= 7) {
+    // Se a média for maior ou igual a 7, exibe uma mensagem de aprovação
+    alert(`${printMediaEstudante(estudante)}. Você passou, parabéns!`);
   } else {
-    return `${estudante.nome} teve média ${media.toFixed(
-      2
-    )} e não passou no concurso.`;
+    // Caso contrário, exibe uma mensagem de reprovação
+    alert(`${printMediaEstudante(estudante)}. Você não passou. Não desista!`);
   }
 }
-
-// Verificar o sucesso de cada estudante e mostrar na tela
-estudantes.forEach((estudante) => {
-  console.log(verificarSucesso(estudante));
-});
